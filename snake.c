94c5761726acd6f860cbd90c6265a30adb081895
pokaichang72 <pokaichang72@gmail.com>
2013-02-23 02:17:17 +0800

    Let the snake moves by it's self

--- snake.c
+++ snake.c
@@ -8,11 +8,11 @@
 #define MAP_Y 20
 #define MAP_EX_WIDTH 2
 
-#define KEY_UP 72
-#define KEY_DOWN 80
-#define KEY_LEFT 75
-#define KEY_RIGHT 77
-#define KEY_EXIT 101
+#define KEY_UP 1
+#define KEY_DOWN 2
+#define KEY_LEFT 3
+#define KEY_RIGHT 4
+#define KEY_QUIT 0
 
 
 int print_map(int map[MAP_X][MAP_Y]) {
@@ -42,8 +42,8 @@ int print_map(int map[MAP_X][MAP_Y]) {
 
 int main() {
 	int i, i2, i3;
-	int key, move;
-	int level=3;
+	int key=KEY_RIGHT, move;
+	int level=3, life=2;
 	int map[MAP_Y][MAP_X] = {
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 		1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
@@ -69,13 +69,20 @@ int main() {
 	int head_x=2, head_y=1, rand_x, rand_y;
 	srand(time(NULL));
 	
-	printf("Snake\n\nUse arrow keys (up, down, left, right) to control.\nPress any key to start.\nPress \"e\" to exit.");
-
+	printf("Snake\n\nUse arrow keys (up, down, left, right) to control.\n\nPress \"e\" to exit.\n");
+	system("PAUSE");
 	while (1) {
-
-		key = getche(); //Get key event
 		move = 0; //if move
 		
+		for(i=0; i<130-(level*3); i++) {
+			Sleep(1);
+			if(GetKeyState(VK_UP) & 0x80 && map[head_y-1][head_x] < 1) key = KEY_UP;
+			if(GetKeyState(VK_DOWN) & 0x80 && map[head_y+1][head_x] < 1) key = KEY_DOWN;
+			if(GetKeyState(VK_RIGHT) & 0x80 && map[head_y][head_x+1] < 1) key = KEY_RIGHT;
+			if(GetKeyState(VK_LEFT) & 0x80 && map[head_y][head_x-1] < 1) key = KEY_LEFT;
+			if(GetKeyState(0x51) & 0x80) key = KEY_QUIT;
+		}
+
 		switch (key) { //Set reactions for key event
 			case KEY_UP:
 				if (head_y > 0 && map[head_y-1][head_x] < 1) {
@@ -105,7 +112,7 @@ int main() {
 				}
 			break;
 			
-			case KEY_EXIT:
+			case KEY_QUIT:
 				return 0;
 			break;
 		}
@@ -138,18 +145,21 @@ int main() {
 		}
 		
 		
-		
 		system("cls");
 
 		print_map(map);
 		printf("Score: %d\n", level-3); //Print Score
+		
+		if (move == 0) life--;
+		else life = 2;
+		
+		if (life == 0) {
+			printf("\nYou are dead.\a\n");
+			system("color 04");
+			break;
+		}
 	}
-
-
-
-
-
-
-
+	
+	system("PAUSE");
 	return 0;
 }
